67bf70982ce50097bed1be77d23026d94c263ff7
jastiling <jastiling@gmail.com>
2022-02-11 09:29:55 -0800

    nft deploy

--- contracts/MyNfts.sol
+++ contracts/MyNfts.sol
@@ -5,8 +5,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNfts is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-        console.log("name", name);
-        console.log("symbol", symbol);
         console.log("msg.sender", msg.sender);
     }
 }
